f2c9f582b0b65821fb8dcb5cd5db20094cf40a4d
unknown <nelsonterdoo1000@gmail.com>
2022-06-19 04:50:39 -0700

    commit after update of settings

--- TERDOONONDO/TERDOONONDO/settings.py
+++ TERDOONONDO/TERDOONONDO/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from ctypes import cast
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gw#gb3$+bsfxohxfng$astu)0m$2*8__7hduzfwlw-53kjxu9u'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
